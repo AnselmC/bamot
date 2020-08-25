b8b4c4d034520d95e45c494e42cd2ec5090dcbbb
Anselm Coogan <anselm.coogan@icloud.com>
2020-08-25 09:33:27 +0200

    drawing keypoints + stereo matches in viewer

--- bamot/util/viewer.py
+++ bamot/util/viewer.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 import open3d as o3d
 
-from bamot.core.base_types import ObjectTrack
+from bamot.core.base_types import Feature, ObjectTrack, Match
 from bamot.util.cv import from_homogeneous_pt, to_homogeneous_pt
 
 LOGGER = logging.getLogger("UTIL:VIEWER")
@@ -17,6 +17,30 @@ Color = Tuple[float, float, float]
 COLORS: List[Color] = list(
     map(lambda col: tuple(col / np.linalg.norm(col)), RNG.random((42, 3), dtype=float))
 )
+
+
+def _draw_features(img: np.ndarray, features: List[Feature]) -> np.ndarray:
+    keypoints = [cv2.KeyPoint(x=f.u, y=f.v, _size=1) for f in features]
+    return cv2.drawKeypoints(img, keypoints, None)
+
+
+def _draw_matches(
+    left_img: np.ndarray,
+    left_features: List[Feature],
+    right_img: np.ndarray,
+    right_features: List[Feature],
+    stereo_matches: List[Match],
+) -> np.ndarray:
+    left_keypoints = [cv2.KeyPoint(x=f.u, y=f.v, _size=1) for f in left_features]
+    right_keypoints = [cv2.KeyPoint(x=f.u, y=f.v, _size=1) for f in right_features]
+    # left is train, right is query?
+    matches = [
+        cv2.DMatch(_queryIdx=leftIdx, _trainIdx=rightIdx, _imgIdx=0, _distance=0)
+        for leftIdx, rightIdx in stereo_matches[::20]
+    ]
+    return cv2.drawMatches(
+        left_img, left_keypoints, right_img, right_keypoints, matches, None
+    )
 
 
 def get_screen_size():
@@ -36,7 +60,7 @@ def _update_tracks(
 ):
     LOGGER.debug("Displaying %d tracks", len(object_tracks))
     for ido, track in object_tracks.items():
-        pose_world_obj = track.poses[len(track.poses) - 1]
+        pose_world_obj = track.poses[max(track.poses.keys())]
         points = []
         for lm in track.landmarks.values():
             points.append(
@@ -50,8 +74,10 @@ def _update_tracks(
         )
         pt_cloud.points = o3d.utility.Vector3dVector(points)
         if track.active:
+            LOGGER.debug("Track is active")
             pt_cloud.paint_uniform_color(color)
         else:
+            LOGGER.debug("Track is inactive")
             pt_cloud.paint_uniform_color([0.0, 0.0, 0.0])
         if track_geometries.get(ido) is None:
             visualizer.add_geometry(pt_cloud)
@@ -78,9 +104,20 @@ def run(shared_data: queue.Queue, stop_flag: Event, next_step: Event):
             LOGGER.debug("Got new data")
             tracks = _update_tracks(tracks, vis, new_data["object_tracks"])
             stereo_image = new_data["stereo_image"]
-            cv2.imshow(
-                cv2_window_name, np.hstack([stereo_image.left, stereo_image.right])
+            left_features = new_data["left_features"]
+            right_features = new_data["right_features"]
+            stereo_matches = new_data["stereo_matches"]
+            # left_img = _draw_features(stereo_image.left, left_features)
+            # right_img = _draw_features(stereo_image.right, right_features)
+            # full_img = np.hstack([left_img, right_img])
+            full_img = _draw_matches(
+                stereo_image.left,
+                left_features,
+                stereo_image.right,
+                right_features,
+                stereo_matches,
             )
+            cv2.imshow(cv2_window_name, full_img)
         keypress = cv2.waitKey(1)
         if keypress == ord("n"):
             next_step.set()
