4d9b0cf9ccb992f51845f674d8b18e36816a2ec1
Anselm Coogan <coogan@srvradig5.informatik.tu-muenchen.de>
2020-09-30 18:55:40 +0200

    added config file to line search

--- run_line_search.sh
+++ run_line_search.sh
@@ -1,3 +1,3 @@
 for cluster_size in {4..12}; do
-    CLUSTER_SIZE=$cluster_size bash run_all_scenes.sh -t cluster_size_$cluster_size
+    CLUSTER_SIZE=$cluster_size bash run_all_scenes.sh -t cluster_size_$cluster_size -c cluster_config.yaml
 done
