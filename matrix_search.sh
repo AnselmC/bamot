b4feb254fa9a6930379cee93c0579648249745f3
Anselm Coogan <anselm.coogan@icloud.com>
2020-10-13 12:33:07 +0200

    added matrix search script

---
+++ matrix_search.sh
@@ -0,0 +1,34 @@
+git update-index --really-refresh
+if ! git diff-index --quiet HEAD; then
+	echo "Git tree is dirty"
+	exit
+fi
+while getopts ":f:t:c:s:e:" arg; do
+	case $arg in
+	t) tags=$OPTARG ;;
+	c) config=$OPTARG ;;
+	esac
+done
+
+for const_motion_weight in {6..14..4}; do
+	for cluster_size in {0..10..4}; do
+		if [ $cluster_size = 0 ]; then
+			for mad_scale_factor in {1..3}; do
+				MAD_SCALE_FACTOR=$mad_scale_factor
+				CONST_MOTION_WEIGHT=$const_motion_weight
+				CLUSTER_SIZE=$cluster_size
+				bash
+				run_all_scenes.sh
+				-c $config
+				-t $tags
+			done
+		else
+			CONST_MOTION_WEIGHT=$const_motion_weight
+			CLUSTER_SIZE=$cluster_size
+			bash
+			run_all_scenes.sh
+			-c $config_file
+			-t $tags
+		fi
+	done
+done
