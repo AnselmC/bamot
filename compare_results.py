85a9b3c2523406da0372c80e7857d73b0ad5ec72
Anselm Coogan <anselm.coogan@icloud.com>
2020-11-03 14:18:36 +0100

    update compare script to use all three main metrics

--- compare_results.py
+++ compare_results.py
@@ -4,6 +4,27 @@ from pprint import pprint
 
 import yaml
 
+MAIN_METRICS = {
+    "median": {
+        "total": ["total", "median"],
+        "car": ["obj-type", "car", "median"],
+        "ped": ["obj-type", "pedestrian", "median"],
+        "reverse": False,
+    },
+    "median-of-object-mean": {
+        "total": ["per-obj", "all", "median-of-mean"],
+        "car": ["obj-type", "car", "all", "median-of-mean"],
+        "ped": ["obj-type", "pedestrian", "all", "median-of-mean"],
+        "reverse": False,
+    },
+    "outlier-ratio": {
+        "total": ["per-obj", "outlier-ratio"],
+        "car": ["obj-type", "car", "outlier-ratio"],
+        "ped": ["obj-type", "pedestrian", "outlier-ratio"],
+        "reverse": False,
+    },
+}
+
 
 def _get_nested(d, keys):
     for key in keys:
@@ -11,38 +32,17 @@ def _get_nested(d, keys):
     return d
 
 
-def _get_n_best(reports, keys, n):
-    medianss = {
-        fname: _get_nested(summary, keys)["median"]
-        for fname, summary in reports.items()
+def _get_n_best(reports, keys, n, reverse=False):
+    flattened = {
+        fname: _get_nested(summary, keys) for fname, summary in reports.items()
     }
-    best_medianss = [(k, v) for k, v in sorted(medianss.items(), key=lambda x: x[1])][
-        :n
-    ]
-    meanss = {
-        fname: _get_nested(summary, keys)["mean"] for fname, summary in reports.items()
+    best = {
+        i + 1: (k, v)
+        for i, (k, v) in enumerate(
+            sorted(flattened.items(), key=lambda x: x[1], reverse=reverse)[:n]
+        )
     }
-    best_meanss = [(k, v) for k, v in sorted(meanss.items(), key=lambda x: x[1])][:n]
-
-    return best_medianss, best_meanss
-
-
-def get_metrics(summary):
-    print("-" * 30)
-    print("Median (total)")
-    print(f'Total: {summary["total"]["median"]:.2f}')
-    print(f'Cars: {summary["obj-type"]["car"]["median"]:.2f}')
-    print(f'Peds: {summary["obj-type"]["pedestrian"]["median"]:.2f}')
-    print("Median (obj means)")
-    print(f"Total: {summary['per-obj']['all']['median-of-mean']:.2f}")
-    print(f"Cars: {summary['obj-type']['car']['all']['median-of-mean']:.2f}")
-    print(
-        f"Pedestrian: {summary['obj-type']['pedestrian']['all']['median-of-mean']:.2f}"
-    )
-    print("Outlier ratio")
-    print(f"Total: {summary['per-obj']['outlier-ratio']:.2f}")
-    print(f"Cars: {summary['obj-type']['car']['outlier-ratio']:.2f}")
-    print(f"Pedestrian: {summary['obj-type']['pedestrian']['outlier-ratio']:.2f}")
+    return best
 
 
 if __name__ == "__main__":
@@ -71,37 +71,15 @@ if __name__ == "__main__":
         print("Need at least two directories/files to compare")
         exit()
 
-    # get fname
-    # filter by
-    for fname, summary in reports.items():
-        print("+" * 30)
-        print(fname)
-        get_metrics(summary)
     n = args.n
-    best_medians, best_means = _get_n_best(reports, ["total"], n)
-    print(f"BEST MEDIANS: {best_medians}")
-    print(f"BEST MEANS: {best_means}")
 
-    best_medians_car, best_means_car = _get_n_best(reports, ["obj-type", "car"], n)
-    print(f"BEST MEDIANS CAR: {best_medians_car}")
-    print(f"BEST MEANS CAR: {best_means_car}")
-
-    best_medians_ped, best_means_ped = _get_n_best(
-        reports, ["obj-type", "pedestrian"], n
-    )
-    print(f"BEST MEDIANS PEDESTRIAN: {best_medians_ped}")
-    print(f"BEST MEANS PEDESTRIAN: {best_means_ped}")
-
-    best_medians_close, best_means_close = _get_n_best(
-        reports, ["distance", "close"], n
-    )
-    print(f"BEST MEDIANS CLOSE: {best_medians_close}")
-    print(f"BEST MEANS CLOSE: {best_means_close}")
-
-    best_medians_mid, best_means_mid = _get_n_best(reports, ["distance", "mid"], n)
-    print(f"BEST MEDIANS MID: {best_medians_mid}")
-    print(f"BEST MEANS MID: {best_means_mid}")
-
-    best_medians_far, best_means_far = _get_n_best(reports, ["distance", "far"], n)
-    print(f"BEST MEDIANS FAR: {best_medians_far}")
-    print(f"BEST MEANS FAR: {best_means_far}")
+    for metric_name, info in MAIN_METRICS.items():
+        print("*" * 30)
+        print(f"Metric: {metric_name.replace('-', ' ').capitalize()}")
+        reverse_order = info.pop("reverse")
+        for obj_type, keys in info.items():
+            print("-" * 30)
+            print(obj_type.capitalize())
+            best = _get_n_best(reports, keys, n, reverse=reverse_order)
+            print(f"{n} best:")
+            pprint(best, indent=4)
