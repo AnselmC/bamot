9a2e29df9d90aa8ec8f45e6e4f5a637402815696
Anselm Coogan <anselm.coogan@icloud.com>
2020-12-02 13:13:27 +0100

    implemented base model for regressor

--- bamot/__init__.py
+++ bamot/__init__.py
@@ -8,6 +8,12 @@ SUPERPOINT_SETTINGS: Path = SUPERPOINT_PATH / "superpoint" / "settings.py"
 # ./setup.sh script in thirdparty `SuperPoint` repo creates `settings.py`.
 # The file won't be there if the script wasn't run and is not necessary
 sys.path.append(SUPERPOINT_PATH.as_posix())
+POINTNET_BASE_PATH: Path = MODULE_PATH / "thirdparty" / "pointnet2"
+POINTNET_MAIN_PATH: Path = POINTNET_BASE_PATH / "pointnet2"
+POINTNET_OPS_PATH: Path = POINTNET_BASE_PATH / "pointnet2_ops"
+sys.path.append(POINTNET_BASE_PATH.as_posix())
+sys.path.append(POINTNET_MAIN_PATH.as_posix())
+sys.path.append(POINTNET_OPS_PATH.as_posix())
 try:
     import superpoint.settings
 except ModuleNotFoundError:
---
+++ bamot/obbox_regressor/train.py
@@ -0,0 +1,34 @@
+import argparse
+
+import pytorch_lightning as pl
+from bamot.obbox_regressor.data.dataloader import BAMOTPointCloudDataModule
+from bamot.obbox_regressor.model import OBBoxRegressor
+from pytorch_lightning.loggers import WandbLogger
+
+
+def main(args):
+    model = OBBoxRegressor(
+        train_batch_size=args.batch_size,
+        eval_batch_size=args.batch_size,
+        max_epochs=args.max_epochs,
+    )
+    dm = BAMOTPointCloudDataModule(
+        dataset_dir=args.input_files_dir,
+    )
+    wandb_logger = WandbLogger()
+    trainer = pl.Trainer(logger=wandb_logger, gpus=args.gpus)
+    trainer.fit(model, dm)
+    trainer.test(datamodule=dm)
+
+
+if __name__ == "__main__":
+    pl.seed_everything(42)
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("input_files_dir", type=str)
+    parser.add_argument(
+        "--max-epochs", type=int, default=5, help="Max epochs (default 5)"
+    )
+    parser.add_argument("-bs", "--batch-size", type=int, default=8)
+    parser.add_argument("--gpus", default=None)
+    args = parser.parse_args()
+    main(args)
