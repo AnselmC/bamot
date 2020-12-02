91ae22313d6dfe0ae0a04902a8606bea9d0394e9
Anselm Coogan <anselm.coogan@icloud.com>
2020-12-02 13:20:48 +0100

    updated eval scripts

--- evaluate_all_trajectories.sh
+++ evaluate_all_trajectories.sh
@@ -1,14 +1,19 @@
-while getopts ":f:t:s:e:" arg; do
+while getopts ":f:t:s:e:g:" arg; do
     case $arg in
         f) feature=$OPTARG;;
         t) tags=$OPTARG;;
         s) start=$OPTARG;;
         e) end=$OPTARG;;
+        g) use_gt=$OPTARG;;
     esac
 done
 
 for scene in $(seq -f "%02g" $start $end)
 do
-    python evaluate_trajectories.py data/KITTI/tracking/training/trajectories/00$scene/$feature/$tags -s data/evaluation/$feature/
+    if [ $use_gt = 1 ]; then
+        python evaluate_trajectories.py data/KITTI/tracking/training/trajectories/00$scene/$feature/$tags -s data/evaluation/$feature/ --track-ids-match
+    else
+        python evaluate_trajectories.py data/KITTI/tracking/training/trajectories/00$scene/$feature/$tags -s data/evaluation/$feature/
+    fi
 done
 python full_evaluation.py data/evaluation/$feature/$tags -s
