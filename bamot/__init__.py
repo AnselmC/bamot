bfc90cb57c578917d7c2f0ff5a8809f5c5ccdeb5
Anselm Coogan <anselm.coogan@icloud.com>
2020-12-02 14:19:00 +0100

    fix for pointnet2

--- bamot/__init__.py
+++ bamot/__init__.py
@@ -10,10 +10,11 @@ SUPERPOINT_SETTINGS: Path = SUPERPOINT_PATH / "superpoint" / "settings.py"
 sys.path.append(SUPERPOINT_PATH.as_posix())
 POINTNET_BASE_PATH: Path = MODULE_PATH / "thirdparty" / "pointnet2"
 POINTNET_MAIN_PATH: Path = POINTNET_BASE_PATH / "pointnet2"
-POINTNET_OPS_PATH: Path = POINTNET_BASE_PATH / "pointnet2_ops"
+POINTNET_OPS_PATH: Path = POINTNET_BASE_PATH / "pointnet2_ops_lib"
 sys.path.append(POINTNET_BASE_PATH.as_posix())
 sys.path.append(POINTNET_MAIN_PATH.as_posix())
 sys.path.append(POINTNET_OPS_PATH.as_posix())
+
 try:
     import superpoint.settings
 except ModuleNotFoundError:
