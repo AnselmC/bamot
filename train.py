8443ef81e26c8f0e94dd4c16867f380aea0a2b4e
Anselm Coogan <anselm.coogan@icloud.com>
2021-05-11 14:32:03 +0200

    added multi-stage flag for training

--- train.py
+++ train.py
@@ -8,6 +8,15 @@ from bamot.obbox_regressor.dataloader import BAMOTPointCloudDataModule
 from bamot.obbox_regressor.model import OBBoxRegressor
 
 
+def get_trainer(args, wandb_logger):
+    return pl.Trainer(
+        logger=wandb_logger,
+        gpus=args.gpus,
+        max_epochs=args.max_epochs,
+        callbacks=[pl.callbacks.EarlyStopping("avg_val_loss")],
+    )
+
+
 def main(args):
     model = OBBoxRegressor(
         train_batch_size=args.batch_size,
@@ -25,14 +34,27 @@ def main(args):
         pointcloud_size=args.num_points,
     )
     wandb.finish()
-    wandb_logger = WandbLogger(project="obbox-regressor", entity="tum-3dmot")
-    trainer = pl.Trainer(
-        logger=wandb_logger,
-        gpus=args.gpus,
-        max_epochs=args.max_epochs,
-        precision=args.precision,
+    wandb_logger = WandbLogger(
+        project="obbox-regressor", entity="tum-3dmot", offline=args.gpus is None
     )
-    trainer.fit(model, dm)
+    if args.multi_stage:
+        # multi-stage training
+        model.train_target = "dim"
+        trainer = get_trainer(args, wandb_logger)
+        trainer.fit(model, dm)
+
+        model.train_target = "pos"
+        trainer = get_trainer(args, wandb_logger)
+        trainer.fit(model, dm)
+
+        model.train_target = "dim"
+        trainer = get_trainer(args, wandb_logger)
+        trainer.fit(model, dm)
+    else:
+        model.train_target = "all"
+        trainer = get_trainer(args, wandb_logger)
+        trainer.fit(model, dm)
+
     trainer.test(datamodule=dm)
 
 
@@ -48,5 +70,6 @@ if __name__ == "__main__":
     parser.add_argument("--gpus", default=None)
     parser.add_argument("--encode-pointcloud", default=False)
     parser.add_argument("--num-points", type=int, default=1024)
+    parser.add_argument("--multi-stage", action="store_true")
     args = parser.parse_args()
     main(args)
